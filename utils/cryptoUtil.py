c2a5b804f05cd1e9e9e0dfc551b4bb1b2aec9fae
Kittiphon <kittiphon.pa.61@ubu.ac.th>
2022-11-05 16:03:14 +0700

    commited

--- utils/cryptoUtil.py
+++
@@ -1,9 +0,0 @@
-from passlib.context import CryptContext
-
-pwd_contex = CryptContext(schemes=["bcrypt"])
-
-def hash_password(password: str):
-    return pwd_contex.hash(password)
-
-def verify_password(plain_password: str, hashed_password: str):
-    return pwd_contex.verify(plain_password, hashed_password)
--- utils/dbUtil.py
+++
@@ -1,9 +0,0 @@
-import databases
-
-from config import cfg
-
-def database_mysql_url_config():
-    return str(cfg.DB_CONNECTION + "://" + cfg.DB_USERNAME + ":" + cfg.DB_PASSWORD +
-               "@" + cfg.DB_HOST + ":" + cfg.DB_PORT + "/" + cfg.DB_DATABASE)
-
-database = databases.Database(database_mysql_url_config())
